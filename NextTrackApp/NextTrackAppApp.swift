5bfbe4639327290cc004d770f2d90ed83a2cf350
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-05 11:00:25 +0300

    Initial Commit

---
+++ NextTrackApp/NextTrackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrackAppApp.swift
+//  NextTrackApp
+//
+//  Created by jopootrivatel on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NextTrackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
