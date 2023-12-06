f27f0270aedd2761af046777934c3b402c7c09f4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-06 14:42:16 +0300

    mission complete

--- NextTrackApp/ContentView.swift
+++ NextTrackApp/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var startAnimate = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button(action: buttonAction) {
+            HStack(spacing: 0) {
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: startAnimate ? 50 : 0, height: startAnimate ? 50 : 0)
+                
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: startAnimate ? 0 : 50, height: startAnimate ? 0 : 50)
+                    .opacity(startAnimate ? 0 : 1)
+            }
+            .foregroundStyle(.tint)
         }
-        .padding()
     }
+    
+    private func buttonAction() {
+        withAnimation(.spring(response: 0.3, dampingFraction: 0.5)) {
+            startAnimate = true
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.startAnimate = false
+        }
+    }
+    
 }
 
 #Preview {
